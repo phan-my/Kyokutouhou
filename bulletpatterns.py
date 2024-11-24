71f29c33d709a1e5f218d9570e8c5cf1b3d6aa30
phan-my <94790649+phan-my@users.noreply.github.com>
2024-11-24 21:45:13 +0000

    Spread out random straight bullets

--- bulletpatterns.py
+++ bulletpatterns.py
@@ -6,18 +6,26 @@ def straight_bullet(sprite, speed) -> None:
     sprite.y += speed
 
 def update_straight_bullet(Bullets, i, speed, playground) -> None:
-    if Bullets[i].y < playground.yBorders[1]:
+    if Bullets[i].y < playground.yBorders[1] or Bullets[i].image != "blank":
         straight_bullet(Bullets[i], speed)
     else:
         Bullets[i].x = 9001
         # Bullets[i].y = playground.yBorders[0]
 
-def random_straight_bullet(Bullets, speed, playground, elapsed):
+def random_straight_bullet(Bullets, bulletCount, nthBullet, playground, ticksSinceStart):
     # clock.schedule_interval(update_straight_bullet, 2.0)
-    bulletsOnScreen = 512
-    interval = 3
-    
+    """            PlayerBullets[nthPlayerBullet].image = "player-bullet-red"
+            nthPlayerBullet = (nthPlayerBullet + 1) % playerBulletCount 
     for k in range(bulletsOnScreen):
-        if (k/interval)*(elapsed/bulletsOnScreen) + 1 >= interval:
-            print((k/interval)*(elapsed/bulletsOnScreen))
-            update_straight_bullet(Bullets, k, speed, playground)
+        if (k/interval)*(elapsed/bulletsOnScreen) + 1 >= interval:"""
+    bulletsOnScreen = bulletCount/2
+    interval = 3
+    speed = 4.5
+    if ticksSinceStart % 2 == 0:
+        Bullets[nthBullet].image = "bullet-vertical"
+        nthBullet = (nthBullet + 1) % bulletCount
+    
+    for bullet in Bullets:
+        if bullet.image != "1x1":
+            straight_bullet(bullet, speed)    
+    return nthBullet
